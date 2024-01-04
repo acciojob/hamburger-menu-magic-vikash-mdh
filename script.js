b4e7dbab095d1d862a0e234c850aab6418f46a7a
VIkash Kumar <103258656+vikash-mdh@users.noreply.github.com>
2024-01-04 14:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    var menuIcon = document.getElementById('menu-icon');
+    var navList = document.getElementById('nav-list');
+
+    menuIcon.addEventListener('click', function () {
+        navList.classList.toggle('active');
+    });
+});
